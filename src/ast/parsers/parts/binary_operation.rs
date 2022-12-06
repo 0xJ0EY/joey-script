73e455cc5641a17ac121ef3bcc4365e83b1d3efe
Joey de Ruiter <contact@joeyderuiter.me>
2022-12-06 23:29:59 +0100

    Added parts/binary_operation parsing

--- src/ast/parsers/parts/binary_operation.rs
+++ src/ast/parsers/parts/binary_operation.rs
@@ -1,6 +1,23 @@
-use crate::{ast::{parser::AstParser, nodes::expression_statement::{BinaryExpression, Expression}, AstParseError}, ast_error, tokenizer::TokenType};
+use crate::{ast::{parser::AstParser, nodes::expression_statement::{BinaryExpression, Expression}, AstParseError, AstErrorType}, ast_error, tokenizer::TokenType};
 
-use super::{parse_non_sequence_expression, parse_binary_operation_expression};
+use super::{function_call::parse_function_call, literal::parse_literal, identifier::parse_identifier};
+
+fn parse_binary_operation_expression(parser: &AstParser, index: usize, tokens_used: &mut usize) -> Result<Expression, AstParseError> {
+    // TODO: Add the other expressions when implemented
+    if let Ok(result) = parse_function_call(parser, index, tokens_used) {
+        return Ok(Expression::CallExpression(result));
+    }
+
+    if let Ok(result) = parse_literal(parser, index, tokens_used) {
+        return Ok(Expression::Literal(result));
+    }
+
+    if let Ok(result) = parse_identifier(parser, index, tokens_used) {
+        return Ok(Expression::Identifier(result));
+    }
+
+    ast_error!(AstErrorType::UnexpectedToken, parser)
+}
 
 fn parse_operator(parser: &AstParser, index: usize, tokens_used: &mut usize) -> Result<String, AstParseError> {
     match parser.token_at(index) {
@@ -19,17 +36,73 @@ fn parse_operator(parser: &AstParser, index: usize, tokens_used: &mut usize) -> 
 
 pub fn parse_binary_operation(parser: &AstParser, index: usize, tokens_used: &mut usize) -> Result<BinaryExpression, AstParseError> {
     let mut tokens = 0;
+    let mut binary_expression;
 
-    let lhs = parse_binary_operation_expression(parser, index + tokens, &mut tokens)?;
-
+    let left = Box::new(parse_binary_operation_expression(parser, index + tokens, &mut tokens)?);
     let operator = parse_operator(parser, index + tokens, &mut tokens)?;
+    let right = Box::new(parse_binary_operation_expression(parser, index + tokens, &mut tokens)?);
 
-    let rhs = parse_binary_operation_expression(parser, index + tokens, &mut tokens)?;
-    
+    binary_expression = BinaryExpression { operator, left, right };
+
+    while let Ok(operator) = parse_operator(parser, index + tokens, &mut tokens) {
+        let left    = Box::new(Expression::BinaryExpression(binary_expression));
+        let right   = Box::new(parse_binary_operation_expression(parser, index + tokens, &mut tokens)?);
+
+        binary_expression = BinaryExpression { operator, left, right };
+    }
+
     *tokens_used += tokens;
 
-    Ok(BinaryExpression { operator, left: Box::new(lhs), right: Box::new(rhs) })
+    Ok(binary_expression)
 }
 
+#[cfg(test)]
+mod tests {
+    use crate::{tokenizer, ast::{parser::AstParser, nodes::expression_statement::Expression}, cast_expression};
 
+    #[test]
+    fn binary_expression_is_binary_operation() {
+        let content = String::from("321 + 123");
 
+        let tokens = tokenizer::parse(&content).unwrap();
+        let parser = AstParser::new(&tokens);
+        let mut tokens_used = 0;
+
+        let result = super::parse_binary_operation(&parser, 0, &mut tokens_used).unwrap();
+
+        let expected_left = cast_expression!(result.left.as_ref(), Expression::Literal).unwrap();
+        let expected_right = cast_expression!(result.right.as_ref(), Expression::Literal).unwrap();
+
+        assert_eq!(result.operator, "+");
+        assert_eq!(expected_left.value.raw, "321");
+        assert_eq!(expected_right.value.raw, "123");
+        assert_eq!(tokens_used, 3);
+    }
+
+    #[test]
+    fn multiple_binary_expression_is_binary_operation() {
+        let content = String::from("112233 - 321 + 123");
+
+        let tokens = tokenizer::parse(&content).unwrap();
+        let parser = AstParser::new(&tokens);
+        let mut tokens_used = 0;
+
+        let result = super::parse_binary_operation(&parser, 0, &mut tokens_used).unwrap();
+
+        let expected_left = cast_expression!(result.left.as_ref(), Expression::BinaryExpression).unwrap();
+
+        let expected_left_left = cast_expression!(expected_left.left.as_ref(), Expression::Literal).unwrap();
+        let expected_left_right = cast_expression!(expected_left.right.as_ref(), Expression::Literal).unwrap();
+
+        let expected_right = cast_expression!(result.right.as_ref(), Expression::Literal).unwrap();
+
+        assert_eq!(expected_left_left.value.raw, "112233");
+        assert_eq!(expected_left.operator, "-");
+        assert_eq!(expected_left_right.value.raw, "321");
+        assert_eq!(result.operator, "+");
+        assert_eq!(expected_right.value.raw, "123");
+
+        assert_eq!(tokens_used, 5);
+    }
+
+}
--- src/ast/parsers/parts/mod.rs
+++ src/ast/parsers/parts/mod.rs
@@ -8,27 +8,6 @@ pub mod sequence;
 pub mod function_call;
 pub mod binary_operation;
 
-fn parse_binary_operation_expression(parser: &AstParser, index: usize, tokens_used: &mut usize) -> Result<Expression, AstParseError> {
-    // TODO: Add the other expressions when implemented
-    // if let Ok(result) = parse_binary_operation(parser, index, tokens_used) {
-    //     return Ok(Expression::BinaryExpression(result));
-    // }
-
-    if let Ok(result) = parse_function_call(parser, index, tokens_used) {
-        return Ok(Expression::CallExpression(result));
-    }
-
-    if let Ok(result) = parse_literal(parser, index, tokens_used) {
-        return Ok(Expression::Literal(result));
-    }
-
-    if let Ok(result) = parse_identifier(parser, index, tokens_used) {
-        return Ok(Expression::Identifier(result));
-    }
-
-    ast_error!(AstErrorType::UnexpectedToken, parser)
-}
-
 fn parse_non_sequence_expression(parser: &AstParser, index: usize, tokens_used: &mut usize) -> Result<Expression, AstParseError> {
     // TODO: Add the other expressions when implemented
     // Out of my head those are ArrayExpression, ObjectExpression, MemberExpression, UpdateExpression
